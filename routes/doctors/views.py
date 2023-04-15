2ef14628aa8e1fe78f94083ad15325412ec88a0e
Abdulrahman Soliman <abdulrahmansoliman391@gmail.com>
2023-04-16 00:33:41 +0200

    added post and patch methods for doctors routes

--- routes/doctors/views.py
+++ routes/doctors/views.py
@@ -1,10 +1,11 @@
 from flask import (Blueprint,jsonify)
 import sys
+from datetime import *
 
 from models.models import Doctor
 from request_errors import requires_body
 
-from routes.doctors.utils import validate_doctor_id
+from routes.doctors.utils import validate_doctor_id, date_handler
 
 doctors_blueprint = Blueprint('doctors', __name__)
 
@@ -28,10 +29,23 @@ def get_doctor(doctor_id):
     }), 200
 
 @doctors_blueprint.route('/', methods=['POST'])
-@requires_body('[username] [password] [email] [first_name] [last_name] [birth_date] [phone_number] [specialization]')
+@requires_body("[username] [password] [email] [first_name] [last_name] [birth_date] [phone_number] [specialization]")
 def create_doctor(data):
     
-    doctor = Doctor(**data)
+    birth_date_obj = date_handler(data)
+
+    doctor_data = {
+        "username": data['username'],
+        "password": data['password'],
+        "email": data['email'],
+        "first_name": data['first_name'],
+        "last_name": data['last_name'],
+        "birth_date": birth_date_obj,
+        "phone_number": data['phone_number'],
+        "specialization": data['specialization']
+    }
+    
+    doctor = Doctor(**doctor_data)
     doctor.insert()
     
     return jsonify({
@@ -39,10 +53,34 @@ def create_doctor(data):
         "doctor": doctor.format()
     }), 201
 
+
 @doctors_blueprint.route('/<int:doctor_id>', methods=['PATCH'])
-def patch_doctor(doctor_id, new_id):
+@requires_body("[username] [password] [email] [first_name] [last_name] [birth_date] [phone_number] [specialization]")
+def update_doctor(data, doctor_id):
     
     validate_doctor_id(doctor_id)
     doctor = Doctor.query.get(doctor_id)
-    doctor.id = new_id
     
+
+    doctor.username = data['username']
+
+    doctor.password = data['password']
+
+    doctor.email = data['email']
+    doctor.first_name = data['first_name']
+
+    doctor.last_name = data['last_name']
+
+    doctor.birth_date = date_handler(data)
+
+    doctor.phone_number = data['phone_number']
+    doctor.specialization = data['specialization']
+    
+    doctor.update()
+    
+    return jsonify({
+        "success": True,
+        "doctor": doctor.format()
+    }), 200
+ 
+ 
