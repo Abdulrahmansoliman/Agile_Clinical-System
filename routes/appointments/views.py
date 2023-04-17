92bc0530c4035cb9b0963afa5b6c3264ec328a9d
Abdulrahman Soliman <abdulrahmansoliman391@gmail.com>
2023-04-17 16:24:48 +0200

    added GET /appointments/{id} route

--- routes/appointments/views.py
+++ routes/appointments/views.py
@@ -1,5 +1,6 @@
 from flask import (Blueprint, abort, jsonify)
 from models.appointments.appointments import Appointment
+from routes.appointments.utils import validate_appointment_id
 
 appointments_blueprint = Blueprint('appointments', __name__)
 
@@ -11,3 +12,14 @@ def get_appointments():
         'data': [a.format() for a in appointments]
     }), 200
 
+
+@appointments_blueprint.route('/<int:appointment_id>', methods=['GET'])
+def get_appointment(appointment_id):
+    validate_appointment_id(appointment_id)
+    appointment = Appointment.query.get(appointment_id)
+    return jsonify({
+        'success': True,
+        'data': appointment.format()
+    }), 200
+    
+    
