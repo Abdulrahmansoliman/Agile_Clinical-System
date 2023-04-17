a9b842717f838d36fe3316720ada639c6314720e
Abdulrahman Soliman <abdulrahmansoliman391@gmail.com>
2023-04-17 16:01:49 +0200

    added patients routes

---
+++ routes/patients/utils.py
@@ -0,0 +1,13 @@
+from flask import abort, jsonify
+
+from models.patients.patients import Patient
+
+def validate_patient_id(patient_id):
+    patient = Patient.query.get(patient_id)
+    if patient is None:
+        error_message = "No patient with id {} found".format(patient_id)
+        print(error_message)
+        abort(jsonify({
+            "status": 404,
+            "message": error_message
+        }))
