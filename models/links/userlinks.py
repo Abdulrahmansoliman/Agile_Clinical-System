f5e95c058c4322b067343617f5bffa3438814226
MostafaWahiep <20p2981@eng.asu.edu.eg>
2023-05-08 23:36:39 +0200

    added userlinks.py

---
+++ models/links/userlinks.py
@@ -0,0 +1,31 @@
+from models.init import db, BaseDbModel
+from models.users.usertypes import UserType
+from models.links.links import Link
+
+class UserLinks(BaseDbModel, db.Model):
+    __tablename__ = 'userlinks'
+
+    linkid = db.Column(db.Integer, db.ForeignKey('link.id'), primary_key=True)
+    usertypeid = db.Column(db.Integer, db.ForeignKey('user_type.id'), primary_key=True)
+
+    link = db.relationship('Link', backref='userlinks', lazy=True)
+
+    def __init__(self, linkid, usertypeid):
+        self.linkid = linkid
+        self.usertypeid = usertypeid
+    
+    def format(self):
+        return {
+            'linkid': self.linkid,
+            'usertypeid': self.usertypeid
+        }
+    
+    def format_with_link(self):
+        return {
+            'link': self.link.format()
+        }
+    
+    def format_with_usertype(self):
+        return {
+            'usertype': self.usertype.format()
+        }
