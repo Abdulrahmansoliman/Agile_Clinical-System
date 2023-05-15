ebef4800883d91f35ac094587fdb9ac82b8f0ec6
Peter Azmy <ehabpeter158@gmail.com>
2023-05-16 01:07:18 +0300

    adding the filteration

--- views/clinic/src/components/Appointments/AppointmentCard.tsx
+++ views/clinic/src/components/Appointments/AppointmentCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/AppointmentCard.css";
 import { Link } from "react-router-dom";
 
@@ -9,12 +9,47 @@ interface AppointmentCardProps {
   notes: string;
 }
 
+const NotesWindow: React.FC<{ notes: string }> = ({ notes }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div>
+      <button onClick={handleToggle}>View Notes</button>
+      <div className="notes-window">
+        {isOpen && (
+          <div className="modal">
+            <div className="modal-content">
+              <h2>Notes</h2>
+              <p>{notes}</p>
+              <button onClick={handleToggle}>Close</button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
 const AppointmentCard: React.FC<AppointmentCardProps> = ({
   patientId,
   doctorId,
   start_date,
   notes,
 }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleModalOpen = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
+
   const formattedDate = new Date(start_date).toLocaleString();
   console.log(start_date);
   console.log(formattedDate);
@@ -24,7 +59,19 @@ const AppointmentCard: React.FC<AppointmentCardProps> = ({
       <h2>Patient ID: {patientId}</h2>
       <p>Doctor ID: {doctorId}</p>
       <p>Date: {formattedDate}</p>
-      <p>Notes: {notes} </p>
+      <NotesWindow notes={notes} />
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <h2>Notes</h2>
+            <p>{notes}</p>
+            <button onClick={handleModalClose}>Close</button>
+          </div>
+        </div>
+      )}
+      <Link to={`/addpatient`}>
+        <button>Profile</button>
+      </Link>
     </div>
   );
 };
--- views/clinic/src/components/Appointments/AppointmentList.tsx
+++ views/clinic/src/components/Appointments/AppointmentList.tsx
@@ -1,6 +1,6 @@
+import React, { useState, useEffect } from "react";
 import AppointmentCard from "./AppointmentCard";
 import "./styles/AppointmentList.css";
-import { useState, useEffect } from "react";
 
 type Appointment = {
   doctor_id: number;
@@ -8,44 +8,65 @@ type Appointment = {
   notes: string;
   patient_id: number;
   secretary_id: number;
-  start_time: string; //may be edited to date type
+  start_time: string; // may be edited to date type
 };
-
-//how to fetch data from patient route based on patient id
-//how to fetch data from doctor route based on doctor id
 
 function AppointmentList() {
   const [appointments, setAppointments] = useState<Appointment[]>([]);
+  const [selectedDate, setSelectedDate] = useState(
+    new Date().toISOString().slice(0, 10) // Default value is today's date
+  );
 
   useEffect(() => {
     fetch("http://127.0.0.1:5000/appointments/", { method: "GET" })
-      .then((response) => {
-        console.log(response);
-        return response.json();
-      })
-
+      .then((response) => response.json())
       .then((data) => {
-        console.log(data.data);
-
         setAppointments(data.data);
       })
-      .catch((error) => console.error("alooooooooo", error.message));
+      .catch((error) => console.error("Error:", error.message));
   }, []);
+
+  const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedDate(event.target.value);
+  };
+
+  const filteredAppointments = appointments.filter((appointment) => {
+    const startTime = new Date(appointment.start_time);
+    const selectedDay = new Date(selectedDate);
+    return (
+      startTime.getDate() === selectedDay.getDate() &&
+      startTime.getMonth() === selectedDay.getMonth() &&
+      startTime.getFullYear() === selectedDay.getFullYear()
+    );
+  });
+
   return (
     <div>
-      <h1 className="appointment-list-h1">Appointment List</h1>
+      <div className="header">
+        <h1 className="appointment-list-h1">Appointment List</h1>
+        <div className="appointment-filter">
+          <label htmlFor="date-input">Select Date:</label>
+          <input
+            type="date"
+            id="date-input"
+            value={selectedDate}
+            onChange={handleDateChange}
+          />
+        </div>
+      </div>
       <div className="appointment-list">
-        {appointments.map((appointments, index) => (
+        {filteredAppointments.map((appointment, index) => (
           <AppointmentCard
             key={index}
-            patientId={appointments.patient_id}
-            doctorId={appointments.doctor_id}
-            start_date={appointments.start_time}
-            notes={appointments.notes}
+            patientId={appointment.patient_id}
+            doctorId={appointment.doctor_id}
+            start_date={appointment.start_time}
+            notes={appointment.notes}
           />
         ))}
       </div>
     </div>
   );
 }
+
 export default AppointmentList;
