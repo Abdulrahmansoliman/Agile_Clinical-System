8c5183969ac33ac08a98692e1c7607f9e243fb9b
MostafaWahiep <20p2981@eng.asu.edu.eg>
2023-05-15 07:29:37 +0200

    added usertype_links relationship

--- models/users/usertype.py
+++ models/users/usertype.py
@@ -1,4 +1,11 @@
 from models.init import db, BaseDbModel
+from models.links.links import Link
+
+usertype_link = db.Table('usertype_link',
+    db.Column('usertype_id', db.Integer, db.ForeignKey('user_type.id'), primary_key=True),
+    db.Column('link_id', db.Integer, db.ForeignKey('link.id'), primary_key=True)
+)
+                          
 
 class UserType(BaseDbModel, db.Model):
     __tablename__ = 'user_type'
@@ -6,6 +13,7 @@ class UserType(BaseDbModel, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     users = db.relationship('User', backref='user_type', lazy=True)
+    linking = db.relationship('Link', secondary=usertype_link, lazy='subquery', backref = 'user_types')
 
     def __init__(self, name):
         self.name = name
