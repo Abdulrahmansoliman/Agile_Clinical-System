d08521e5fecfa25fe21302c63c3f31687d51c105
MostafaWahiep <20p2981@eng.asu.edu.eg>
2023-05-21 00:16:08 +0200

    added is_deleted flag to all classes

--- models/patients/patients.py
+++ models/patients/patients.py
@@ -15,16 +15,14 @@ class Patient(BaseDbModel, db.Model):
     birth_date = db.Column(db.Date, nullable=False)
     phone_number = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), nullable=False)
-    deleted = db.Column(db.Boolean, default=False)
     appointments = db.relationship('Appointment', backref='patient', lazy=True)
 
-    def __init__(self, first_name, last_name, birth_date, phone_number, email, deleted=False):
+    def __init__(self, first_name, last_name, birth_date, phone_number, email):
         self.first_name = first_name
         self.last_name = last_name
         self.birth_date = birth_date
         self.phone_number = phone_number
         self.email = email
-        self.deleted = deleted
 
     def format(self):
         # Return a formatted dictionary representation of the object
@@ -35,7 +33,6 @@ class Patient(BaseDbModel, db.Model):
             'birth_date': self.birth_date,
             'phone_number': self.phone_number,
             'email': self.email,
-            'deleted': self.deleted,
             'appointments': [a.id for a in self.appointments]
         }
         return formatted_dict
