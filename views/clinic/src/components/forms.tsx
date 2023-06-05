5b95d30342e181823e57b98d8064af480d0d2a4c
Peter Azmy <ehabpeter158@gmail.com>
2023-06-05 18:55:26 +0300

    commit login

--- views/clinic/src/components/forms.tsx
+++ views/clinic/src/components/forms.tsx
@@ -2,23 +2,62 @@ import React, { useState } from "react";
 import "./styles/forms.css";
 
 interface LoginFormProps {
-  onFormSubmit: (formData: { email: string; password: string }) => void;
-  onForgotPasswordClick: () => void;
-  onRegisterClick: () => void;
+  onusernameClick: () => void;
 }
 
-export const LoginForm: React.FC<LoginFormProps> = ({
-  onFormSubmit,
-  onForgotPasswordClick,
-  onRegisterClick,
-}) => {
+export const LoginemailForm = ({ onusernameClick }: LoginFormProps) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  let usertype = "email";
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    // Form validation and submission logic
-    onFormSubmit({ email, password });
+
+    // Form validation
+    if (!email) {
+      setEmailError("Email is required");
+    } else {
+      setEmailError("");
+    }
+
+    if (!password) {
+      setPasswordError("Password is required");
+    } else {
+      setPasswordError("");
+    }
+
+    // Check if there are no errors before submitting the form
+    if (email && password) {
+      sendFormRequest({ usertype, email, password });
+    }
+  };
+
+  const sendFormRequest = (formData: {
+    usertype: string;
+    email: string;
+    password: string;
+  }) => {
+    console.log(formData);
+    fetch(`http://127.0.0.1:5000/auth/login`, {
+      method: "POST",
+      mode: "no-cors",
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the response data
+        console.log(data);
+      })
+      .catch((error) => {
+        // Handle any errors
+        console.error(error);
+      });
+  };
+
+  const handleusernameClick = () => {
+    onusernameClick();
   };
 
   return (
@@ -32,6 +71,7 @@ export const LoginForm: React.FC<LoginFormProps> = ({
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
+        {emailError && <p className="error">{emailError}</p>}
         <label htmlFor="password">Password:</label>
         <input
           type="password"
@@ -39,86 +79,95 @@ export const LoginForm: React.FC<LoginFormProps> = ({
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
+        {passwordError && <p className="error">{passwordError}</p>}
         <button type="submit">Login</button>
       </form>
       <p>
-        <button onClick={onForgotPasswordClick}>Forgot Password</button>
-      </p>
-      <p>
-        Don't have an account?{" "}
-        <button onClick={onRegisterClick}>Register</button>
-      </p>
-    </div>
-  );
-};
-
-interface ForgotPasswordFormProps {
-  onFormSubmit: (formData: { email: string }) => void;
-  onBackToLoginClick: () => void;
-}
-
-export const ForgotPasswordForm: React.FC<ForgotPasswordFormProps> = ({
-  onFormSubmit,
-  onBackToLoginClick,
-}) => {
-  const [email, setEmail] = useState("");
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    // Form validation and submission logic
-    onFormSubmit({ email });
-  };
-
-  return (
-    <div>
-      <h2>Forgot Password</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <button type="submit">Submit</button>
-      </form>
-      <p>
-        <button onClick={onBackToLoginClick}>Back to Login</button>
+        login with username?{" "}
+        <button onClick={handleusernameClick}>username</button>
       </p>
     </div>
   );
 };
 
 interface RegisterFormProps {
-  onFormSubmit: (formData: { email: string; password: string }) => void;
-  onBackToLoginClick: () => void;
+  onBackToemailClick: () => void;
 }
 
-export const RegisterForm: React.FC<RegisterFormProps> = ({
-  onFormSubmit,
-  onBackToLoginClick,
-}) => {
-  const [email, setEmail] = useState("");
+export const LoginusernameForm = ({
+  onBackToemailClick,
+}: RegisterFormProps) => {
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [usernameError, setUsernameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  let userType = "username"; // Change variable name to 'userType'
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    // Form validation and submission logic
-    onFormSubmit({ email, password });
+
+    // Form validation
+    if (!username) {
+      // Change 'email' to 'username'
+      setUsernameError("Username is required"); // Change error message
+    } else {
+      setUsernameError("");
+    }
+
+    if (!password) {
+      setPasswordError("Password is required");
+    } else {
+      setPasswordError("");
+    }
+
+    // Check if there are no errors before submitting the form
+    if (username && password) {
+      // Change 'email' to 'username'
+      sendFormRequest({ userType, username, password }); // Change 'usertype' to 'userType'
+    }
+  };
+
+  const sendFormRequest = (formData: {
+    userType: string; // Change field name to 'userType'
+    username: string; // Change field name to 'username'
+    password: string;
+  }) => {
+    console.log(formData);
+    fetch(`http://127.0.0.1:5000/auth/login`, {
+      method: "POST",
+      mode: "no-cors",
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the response data
+        console.log(data);
+      })
+      .catch((error) => {
+        // Handle any errors
+        console.error(error);
+      });
+  };
+
+  const handleBackToemailClick = () => {
+    // Change function name to 'handleUsernameClick'
+    onBackToemailClick();
   };
 
   return (
     <div>
-      <h2>Register</h2>
+      <h2>Login</h2>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email:</label>
+        <label htmlFor="username">Username:</label>{" "}
+        {/* Change 'email' to 'username' */}
         <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
         />
+        {usernameError && <p className="error">{usernameError}</p>}{" "}
+        {/* Change variable name */}
         <label htmlFor="password">Password:</label>
         <input
           type="password"
@@ -126,24 +175,18 @@ export const RegisterForm: React.FC<RegisterFormProps> = ({
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <label htmlFor="confirmPassword">Confirm Password:</label>
-        <input
-          type="password"
-          id="confirmPassword"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-        />
-        <button type="submit">Register</button>
+        {passwordError && <p className="error">{passwordError}</p>}
+        <button type="submit">Login</button>
       </form>
       <p>
-        <button onClick={onBackToLoginClick}>Back to Login</button>
+        Login with email?{" "}
+        <button onClick={handleBackToemailClick}>Email</button>{" "}
       </p>
     </div>
   );
 };
 
 export default {
-  LoginForm,
-  ForgotPasswordForm,
-  RegisterForm,
+  LoginemailForm,
+  LoginusernameForm,
 };
